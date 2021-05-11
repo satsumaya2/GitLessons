00aae144f94582a54a18e592d1d7e1e311bf08ff
H.SATSU <h-satsu@kip.co.jp>
2021-05-11 13:16:31 +0900

    出力メッセージを更新する

--- GitHubConsole1/Program.cs
+++ GitHubConsole1/Program.cs
@@ -2,11 +2,11 @@
 
 namespace GitHubConsole1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
         }
     }
 }
