829da448395302aa871c13021e1b1031ba5fd37c
H.SATSU <h-satsu@kip.co.jp>
2021-05-11 09:15:17 +0900

    プロジェクト ファイルを追加します。

---
+++ GitHubConsole1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
